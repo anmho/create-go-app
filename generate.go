e9c88c76e9f26a3bc371c9a8eca0761c1b0697eb
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-12-25 20:46:46 -0800

    comment out some stuff

---
+++ generate.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+//go:embed templates/*
+var templateFs embed.FS
+
+func replaceDirWithValues(dir string, opts options) {
+	outDir := opts.appName
+	// err := os.Mkdir(outDir, 0777)
+	// if err != nil {
+	// 	log.Fatalln("directory already exists here")
+	// }
+	
+	// dirs, err := fs.ReadDir(templateFs, dir)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(dirs)
+
+	fs.WalkDir(templateFs, dir, func(path string, d fs.DirEntry, err error) error {
+		// fmt.Println(dir, path)
+		if err != nil {
+			panic(err)
+		}
+		
+		parts := []string{}
+		parts = append(parts, outDir)
+		parts = append(parts, strings.Split(path, "/")[2:]...)
+		// fmt.Println(parts)
+		outPath := filepath.Join(parts...)
+		if d.IsDir() {
+			err := os.Mkdir(outPath, 0777)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			tmpl, err := template.ParseFS(templateFs, path)
+			if err != nil {
+				panic(err)
+			}
+			if strings.HasSuffix(outPath, ".tmpl") {
+				outPath = strings.Replace(outPath, ".tmpl", "", -1)
+			}
+			file, err := os.Create(outPath)
+			if err != nil {
+				panic(err)
+			}
+			err = tmpl.Execute(file, struct{ Name string }{Name: "andy"})
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		return nil
+	})
+
+}
+
+func generateTemplatedAPI(opts options) {
+	dir := "templates/connectrpc-cloudrun"
+	replaceDirWithValues(dir, opts)
+}
+
+
+func generateTemplatedMain(opts options) {
+	// pwd, err := os.Getwd()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	templateDir := "templates"
+	outDir := "out"
+
+	fs.WalkDir(templateFs, templateDir, func(path string, d fs.DirEntry, err error) error {
+		// fmt.Println(path)
+		parts := strings.Split(path, "/")
+		parts[0] = outDir
+		outPath := filepath.Join(parts...)
+		if d.IsDir() {
+			os.Mkdir(outPath, 0777)
+		} else {
+			tmpl, err := template.ParseFS(templateFs, path)
+			if err != nil {
+				panic(err)
+			}
+			file, err := os.Create(outPath)
+			if err != nil {
+				panic(err)
+			}
+			err = tmpl.Execute(file, struct{ Name string }{Name: "andy"})
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		return nil
+	})
+}
--- main.go
+++ main.go
@@ -1,51 +1,82 @@
 package main
 
+// A simple example that shows how to retrieve a value from a Bubble Tea
+// program after the Bubble Tea has exited.
+
 import (
 	"embed"
 	"fmt"
 	"io/fs"
-	"os"
-	"path/filepath"
-	"strings"
-	"text/template"
+	"log"
+
+	"github.com/charmbracelet/huh"
 )
 
-//go:embed templates/*
-var templateFs embed.FS
+type options struct {
+	appName string
+	appTemplate string
+}
+
+func NewOptions() options {
+	return options{
+		appName: "my-go-app",
+	}
+}
+
+var (
+	confirmConfig bool
+)
+
+type AppTemplate string
+
+const (
+	HumaFlyIO       AppTemplate = "huma-flyio"
+	ConnectCloudRun AppTemplate = "connect-cloudrun"
+)
+
+//go:embed all:templates/*
+var templateFs2 embed.FS
 
 func main() {
-	pwd, err := os.Getwd()
+	dirs, err := fs.ReadDir(templateFs2, "templates/connectrpc-cloudrun")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("pwd", pwd)
-	templateDir := "templates"
-	outDir := "out"
-	fs.WalkDir(templateFs, templateDir, func(path string, d fs.DirEntry, err error) error {
-		fmt.Println(path)
-		parts := strings.Split(path, "/")
-		parts[0] = outDir
-		outPath := filepath.Join(parts...)
-		if d.IsDir() {
-			os.Mkdir(outPath, 0777)
-		} else {
-            tmpl, err := template.ParseFS(templateFs, path)
-            if err != nil {
-                panic(err)
-            }
-            file, err := os.Create(outPath)
-            // file, err := os.OpenFile(outPath, os.O_CREATE|os.O_WRONLY, 0666)
-            if err != nil {
-                panic(err)
-            }
-            err = tmpl.Execute(file, struct{Name string}{Name: "andy"})
-            if err != nil {
-                panic(err)
-            }
-        }
+	fmt.Println(dirs)
+	
+	opts := NewOptions()
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("What is the name of your app?").
+				Placeholder("my-go-app").
+				Value(&opts.appName),
+		),
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Choose your API Framework (API framework + PaaS)").
+				Options(
+					huh.NewOption("HumaRocks + fly.io", string(HumaFlyIO)),
+					huh.NewOption("ConnectRPC + Google Cloud Run", string(ConnectCloudRun)),
+				).
+				Value(&opts.appTemplate), // store the chosen option in the "burger" variable
+		),
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Confirm configuration?").
+				Description(fmt.Sprintf(
+`
+	Name: %s
+	Template: %s`, opts.appName, opts.appTemplate)).
+				Value(&confirmConfig),
+		),
+	)
+	
+	if err := form.Run(); err != nil {
+		log.Fatal(err)
+	}
 
-		return nil
-	})
-
+	// generateTemplatedMain(opts)
+	generateTemplatedAPI(opts)
 }
